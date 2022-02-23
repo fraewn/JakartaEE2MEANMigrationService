7a8c72da00b65c586c8a3bca5da790bd7041bded
Franziska Kuesters <franziska.kuesters@smail.inf.h-brs.de>
2022-02-23 18:49:34 +0100

    added code generation for backend controllers

--- src/main/java/com/migration/service/service/codeGeneration/GenerateCode.java
+++ src/main/java/com/migration/service/service/codeGeneration/GenerateCode.java
@@ -24,6 +24,8 @@ public class GenerateCode {
 	private NodeKnowledgeService nodeKnowledgeService;
 	private OntologyKnowledgeService ontologyKnowledgeService;
 	private EntityModelService entityModelService;
+	String modelPackage;
+	String controllerPackage;
 	List<EntityModel> entityModels = new ArrayList<>();
 
 	String path = "";
@@ -43,8 +45,101 @@ public class GenerateCode {
 		this.path = path.replaceAll("/", "\\\\");
 		this.createBackendDirectory();
 		this.createModels();
+		this.createRestControllers();
 	}
 
+	// ######################### Rest Controllers ####################
+	public List<Node> getAllRestControllers(){
+		String query = "MATCH (m:RestController) return m";
+		Driver driver = setUpNeo4jDriver("MEAN");
+		try(Session session = setUpNeo4jSession(driver)) {
+			return session.run(query).list(result -> result.get("m").asNode());
+		}
+	}
+
+	public List<String> getFunctionalitiesAssociatedWithRestController(String id){
+		String query = "MATCH(m:RestController)-[]-(f:Functionality) where m.id='" + id + "' return f";
+		Driver driver = setUpNeo4jDriver("MEAN");
+		try(Session session = setUpNeo4jSession(driver)) {
+			return session.run(query).list(result -> result.get("f").asNode().get("name").asString());
+		}
+	}
+
+	public List<String> getOtherRestControllersAssociatedWithRestController(String id){
+		String query = "MATCH(m:RestController)-[]-(f:RestController) where m.id='" + id + "' return f";
+		Driver driver = setUpNeo4jDriver("MEAN");
+		try(Session session = setUpNeo4jSession(driver)) {
+			return session.run(query).list(result -> result.get("f").asNode().get("id").asString());
+		}
+	}
+
+	public List<String> getComponentsAssociatedWithServiceThatUsesRoute(String id){
+		String query = "match(rc:RestController)-[]-(r:Route)-[]-(s:Service)-[]-(m:Interface)-[]-(c:Component) where rc.id='" + id + "' " +
+				"return c";
+		String queryForControllersWithMiddleware = "match(rc:RestController)-[]-(k:Middleware)-[]-(r:Route)-[]-(s:Service)-[]-" +
+				"(m:Interface)-[]-(c:Component) where rc.id='" + id + "' " +
+				"return c";
+		Driver driver = setUpNeo4jDriver("MEAN");
+		try(Session session = setUpNeo4jSession(driver)) {
+
+			return session.run(query).list(result -> result.get("c").asNode().get("id").asString()).size()==0 ?
+					session.run(queryForControllersWithMiddleware).list(result -> result.get("c").asNode().get("id").asString()) :
+					session.run(query).list(result -> result.get("c").asNode().get("id").asString());
+		}
+	}
+
+	public List<String> getModelByController(String id){
+		String query = "match(rc:RestController)-[]-(m:Model) where rc.id='" + id + "' " +
+				"return m";
+		Driver driver = setUpNeo4jDriver("MEAN");
+		try(Session session = setUpNeo4jSession(driver)) {
+			return session.run(query).list(result -> result.get("m").asNode().get("id").asString());
+		}
+	}
+
+	public void createRestControllers(){
+		List<Node> controllerNodes = this.getAllRestControllers();
+		for(Node node : controllerNodes){
+			String filename = node.get("id").asString();
+			String packageName = "\\" + node.get("package").asString() + "\\";
+			System.out.println("++++++ Code generation for file: " + filename + " at location: " + this.path + packageName + " +++++\n");
+			this.generateRestControllerCode(filename);
+		}
+	}
+
+	public String removeComponentPartsFromName(String name){
+		return name.replace(".component.ts","");
+	}
+
+	public void generateRestControllerCode(String filename){
+		List<String> functionalities = this.getFunctionalitiesAssociatedWithRestController(filename);
+		List<String> otherAssociatedRestControllers = this.getOtherRestControllersAssociatedWithRestController(filename);
+		List<String> neededMethodsForComponent = this.getComponentsAssociatedWithServiceThatUsesRoute(filename);
+		String importedFunctionalities = "";
+		for(String func : functionalities){
+			importedFunctionalities = importedFunctionalities + "const " + func.replace(" ", "") + " = require('" + func + "');\n";
+		}
+		String otherControllerImport = "";
+		importedFunctionalities = importedFunctionalities + "\n";
+		for(String controller : otherAssociatedRestControllers){
+			String model = this.getModelByController(controller).get(0).replace(".js", "");
+			otherControllerImport =
+					otherControllerImport + "const " + model + " = require('../" + this.modelPackage + "/" + model.toLowerCase() + "');" +
+							"\n";
+		}
+		String model = this.getModelByController(filename).get(0).replace(".js", "");
+		String ownControllerImport = "const " +  model + " = require('../" + this.modelPackage + "/" + model.toLowerCase() + "');\n\n";
+
+		String methods = "";
+		for(String method : neededMethodsForComponent){
+			methods = methods + "exports." + this.removeComponentPartsFromName(method.replace("-","")) + " = (req, res, next) => { \n// " +
+					"TODO: Implement method  \n}\n";
+		}
+		String code = importedFunctionalities + otherControllerImport + ownControllerImport + methods;
+		System.out.println(code);
+	}
+
+	// ######################### Models ##############################
 	public List<Node> getAllModels(){
 		String query = "MATCH (m:Model) return m";
 		Driver driver = setUpNeo4jDriver("MEAN");
@@ -66,8 +161,10 @@ public class GenerateCode {
 		List<Node> modelNodes = this.getAllModels();
 		for(Node node : modelNodes){
 			String filename = node.get("id").asString();
-			String packageName = node.get("package").asString();
+			this.modelPackage = node.get("package").asString();
+			String packageName = "\\" + node.get("package").asString() + "\\";
 			String functionality = this.getFunctionalitiesAssociatedWithModel(filename).get(0);
+			System.out.println("++++++ Code generation for file: " + filename + " at location: " + this.path + packageName + " +++++\n");
 			this.generateModelCode(functionality, filename);
 
 		}
@@ -90,11 +187,23 @@ public class GenerateCode {
 			attributeDefintion = attributeDefintion + entry.getKey() + ": {type: " + entry.getValue() + ", required: true},\n";
 		}
 		// cut off last comma
-		attributeDefintion = attributeDefintion.substring(0, attributeDefintion.length()-2) + "\n\n";
+		attributeDefintion = attributeDefintion.substring(0, attributeDefintion.length()-2) + "\n});\n\n";
 		String moduleExport =
-				"module.exports = " + functionality + ".model('" + entityName.replace(".java", "") + "'," + this.transformJSFilenameToPureEntity(filename) + "Schema)";
+				"module.exports = " + functionality + ".model('" + entityName.replace(".java", "") + "', " + this.transformJSFilenameToPureEntity(filename) + "Schema)";
 		String code = functionalityImport + schemaDefintion + attributeDefintion + moduleExport;
-		System.out.println(code);
+		System.out.println(code+"\n\n\n");
+	}
+
+
+
+
+
+
+
+
+
+	public void generateControllerCode(String filename){
+
 	}
 
 	public String transformJSFilenameToPureEntity(String filename){
